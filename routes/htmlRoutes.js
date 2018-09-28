26c8c043b9be7ae88d6f058b18a5a37280b5d20a
Cassindi <tara.devin.b@gmail.com>
2018-09-27 19:44:04 -0500

    comment page

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -16,7 +16,7 @@ router.get("/saved", function(req, res) {
 
 router.get("/comments/:id", function (req, res) {
     Scrape.find({_id: req.params.id},function(err,data) {
-        res.render("comments", {scrape: data})
+        res.render("scrape", {scrape: data})
     })
 });
 
